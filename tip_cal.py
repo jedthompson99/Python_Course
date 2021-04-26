b205bb8483c1e846885ed83c5d206a4ea1d1a2be
jedthompson99 <jedthompson@gmail.com>
2021-04-26 09:07:25 -0600

    Tip Calculator

---
+++ tip_cal.py
@@ -0,0 +1,16 @@
+name = 'Kristine'
+post_count = 42
+
+print(name)
+print(post_count)
+
+meal_completed = True
+sub_total = 100
+tip = sub_total * 0.2
+print (tip)
+
+total = sub_total + tip
+print(total)
+
+receipt = "Your total is" + total
+print(receipt)
