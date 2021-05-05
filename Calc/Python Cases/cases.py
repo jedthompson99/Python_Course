e80f8df79920c6eebab084519829bf15365f058d
jedthompson99 <jedthompson@gmail.com>
2021-05-05 07:49:09 -0600

    Python Basics

---
+++ Calc/Python Cases/cases.py
@@ -0,0 +1,11 @@
+sentence = 'The quick brown fox jumped'.upper()
+
+print(sentence)
+
+sentence_2 = 'the quick brown fox jumped'.capitalize()
+sentence_3 = 'the quick brown fox jumped'.title()
+sentence_4 = 'the quick brown fox JUMPED'.lower()
+
+print(sentence_2)
+print(sentence_3)
+print(sentence_4)
--- tip_cal.py
+++ tip_cal.py
@@ -1,16 +1,27 @@
-name = 'Kristine'
-post_count = 42
+# name = 'Kristine'
+# post_count = 42
 
-print(name)
-print(post_count)
+# print(name)
+# print(post_count)
 
-meal_completed = True
-sub_total = 100
-tip = sub_total * 0.2
-print (tip)
+# meal_completed = True
+# sub_total = 100
+# tip = sub_total * 0.2
+# print (tip)
 
-total = sub_total + tip
-print(total)
+# total = sub_total + tip
+# print(total)
 
-receipt = "Your total is" + total
-print(receipt)
+# receipt = "Your total is" + total
+# print(receipt)
+
+
+first = 'Springer'
+second = 'Bregman'
+third = 'Altuve'
+
+print(second)
+
+second = 'Correa'
+
+print(second)
