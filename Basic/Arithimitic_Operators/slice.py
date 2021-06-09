dafb7349d0055a78acd427bc4d0392bbf1205d87
Jed Thompson <jedthompson@gmail.com>
2021-06-09 09:20:19 -0600

    More Python

---
+++ Basic/Arithimitic_Operators/slice.py
@@ -0,0 +1,7 @@
+tags = ['python', 'development', 'tutorials',
+        'code', 'programming', 'computer science']
+
+
+tag_range = tags[1:-1:2]
+
+print(tag_range)
---
+++ Basic/Arithimitic_Operators/sorted.py
@@ -0,0 +1,10 @@
+# sale_prices = [1, 100, 83, 499, 3, 56]
+
+# sorted_list = sorted(sale_prices)
+# print(sorted_list)
+
+
+sale_prices = [1, 100, 83, 499, 3, 56]
+
+sorted_list = sorted(sale_prices, reverse=True)
+print(sorted_list)
