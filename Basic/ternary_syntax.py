4cf90d4dfda75344c2b068e579857c0de88faee9
Jed Thompson <jedthompson@gmail.com>
2021-07-01 08:28:34 -0600

    Ternary Syntax

---
+++ Basic/ternary_syntax.py
@@ -0,0 +1,17 @@
+# role = 'guest'
+
+# auth = 'can access' if role == 'admin' else 'cannot access'
+
+# if role == 'admin':
+#   auth = 'can access'
+# else:
+#   auth = 'cannot access'
+
+# print(auth)
+
+language = "not python"
+
+language_check = 'True' if language == 'python' else "False"
+
+print(language_check)
+
