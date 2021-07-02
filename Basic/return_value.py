b5f5d9a412ad8017210f6afb4e4b2c204b1eaf85
Jed Thompson <jedthompson@gmail.com>
2021-07-02 08:35:38 -0600

    return values

---
+++ Basic/return_value.py
@@ -0,0 +1,21 @@
+# def full_name(first, last):
+#   return f'{first} {last}'
+
+
+# kristine = full_name('Kristine', 'Hudgens')
+
+# def greeting(name):
+#   print(f'Hi {name}!')
+
+
+# greeting(kristine)
+
+
+#CODING EXERCISE
+
+def sum_two_numbers(a,b):
+        sum = a + b
+        return(sum)
+    
+sum_two_numbers(1,2)
+
