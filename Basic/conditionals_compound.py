0ee14f7d7c35ac5523f34705e4d66a21ae32535e
Jed Thompson <jedthompson@gmail.com>
2021-07-02 07:36:58 -0600

    compound conditionals

---
+++ Basic/conditionals_compound.py
@@ -0,0 +1,39 @@
+# username = 'jonsnow'
+# email = 'jon@snow.com'
+# password = 'thenorth'
+
+# if username == 'jonsnow' and password == 'thenorth':
+#   print('Access permitted')
+# else:
+#   print('Not allowed')
+
+
+# if (username == 'jonsnow' or email == 'jon@snow.com') and password == 'thenorth':
+#   print('Access permitted')
+# else:
+#   print('Not allowed')
+
+
+# if username == 'jonsnow' or password == 'thenorth':
+#   print('Access permitted')
+# else:
+#   print('Not allowed')
+
+
+# logged_in = True
+# standard_user = False
+
+# if logged_in and not standard_user:
+#   print('You can access the admin dashboard')
+# else:
+#   print('You can only access the standard dashboard')
+
+#CODING EXERCISE
+
+def compound_conditional(number):
+    if number > 0 and number < 101:
+        print("Success!")
+    else:
+        print("Failure...")
+
+compound_conditional(101)
